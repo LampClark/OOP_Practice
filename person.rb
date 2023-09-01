07b46b1d4745bebcadfbeca9824be1922a79dd85
Charles Lee <unknownsanon@gmail.com>
2023-09-01 14:00:25 +0800

    Add some color

--- person.rb
+++ person.rb
@@ -1,6 +1,7 @@
 # Create a Ruby class called Person with attributes name and age. 
 # Include a method to compare ages of two people.
 
+require 'colorize'
 class Person
     attr_accessor :name, :age
     
@@ -11,13 +12,13 @@ class Person
 
     def compare_age(other_person)
         if @age > other_person.age
-            "#{@name} is older than #{other_person.name}"
+            "#{@name} is older than #{other_person.name}".light_cyan
 
         elsif @age < other_person.age
-            "#{@name} is younger than #{other_person.name}" 
+            "#{@name} is younger than #{other_person.name}".light_cyan
             
         else
-            "#{@name} and #{other_person.name} are the same age"
+            "#{@name} and #{other_person.name} are the same age".light_cyan
         end
     end
 end
@@ -27,10 +28,10 @@ person2 = Person.new("Rei", 30)
 person3 = Person.new("Sophie", 33)
 person4 = Person.new("Clara", 33)
 
-puts "Name: #{person1.name} | Age: #{person1.age}"
-puts "Name: #{person2.name} | Age: #{person2.age}"
-puts "Name: #{person3.name} | Age: #{person3.age}"
-puts "Name: #{person4.name} | Age: #{person4.age}"
+puts "Name: #{person1.name} | Age: #{person1.age}".light_green
+puts "Name: #{person2.name} | Age: #{person2.age}".light_green
+puts "Name: #{person3.name} | Age: #{person3.age}".light_green
+puts "Name: #{person4.name} | Age: #{person4.age}".light_green
 puts "------------------------------------------------------"
 puts person1.compare_age(person2)
 puts person2.compare_age(person1)
