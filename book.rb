ed4bedd8ac3c960e64eb8b1138f01a3b12993dfa
Charles Lee <unknownsanon@gmail.com>
2023-09-01 15:41:11 +0800

    book completed

---
+++ book.rb
@@ -0,0 +1,18 @@
+require 'colorize'
+
+class Book
+    attr_accessor :title, :author, :pages
+
+    def initialize(title, author, pages)
+        @title = title
+        @author = author
+        @pages = pages
+    end
+
+    def display_info
+        "Title: #{@title} | Author: #{@author} | Pages: #{@pages}".light_red
+    end
+end
+
+book = Book.new("Forbidden Words", "Rei", 1840)
+puts book.display_info
